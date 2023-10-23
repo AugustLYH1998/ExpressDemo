4f9cf787c05418596c468518a9d297ced85dec89
AugustLYH1998 <60884610+AugustLYH1998@users.noreply.github.com>
2023-10-23 11:49:08 +0800

    demo

--- apiRouter.js
+++ apiRouter.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const jwt = require('jsonwebtoken')
+const menus = require('./menus')
 
 // 在这里挂载对应的路由
 router.get('/get', (req, res) => {
@@ -36,8 +38,9 @@ router.delete('/delete', (req, res) => {
 
 // test
 router.get('/get/arr', (req, res) => {
+    // console.log(res);
     //设置允许跨域的域名,*代表允许任意域名跨域
-    res.setheader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Origin", "*")
 
     res.send({
         status: 0,
@@ -52,6 +55,54 @@ router.get('/get/arr', (req, res) => {
     })
 })
 
+//  /
+router.get('/', (req, res) => {
+    res.send('hello')
+})
 
+// /refresh_token get {token}
+let t = Math.random(new Date())
+router.get('/refresh_token', (req, res) => {
+    res.json({ token: ++t })
+})
+
+
+// login
+// /login post password,username { code: 200, message: '登录成功', token: token }
+router.post('/login', (req, res) => {
+    let data = ''
+    req.on('data', (chunk) => {
+        console.log(chunk);
+        data += chunk
+    })
+
+    req.on('end', () => {
+        data = decodeURI(data)
+        try {
+            data = JSON.parse(data);
+        } catch (e) {
+            let eles = data.split('&');
+            data = {};
+            eles.forEach(kv => {
+                let k = kv.split('=')[0];
+                let v = kv.split('=')[1];
+                data[k] = v;
+            })
+        }
+        console.log(data);
+        if (!data.username || !data.password) {
+            return res.send({ code: 403, message: '必须传递用户名密码' })
+        } else if (data.username !== 'admin' || data.password !== '123456') {
+            return res.send({ code: 403, message: '用户名或密码不正确' })
+        }
+        let msg = 'lyh'
+        let token = jwt.sign(data, msg)
+        res.send({ code: 200, message: '登录成功', token: token })
+    })
+})
+
+router.get('/menus/build', (req, res) => {
+    res.json(menus)
+})
 
 module.exports = router
--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express')
 // 创建服务器实例
 const app = express()
 
+/* 可以处理表单的数据 */
 // 配置解析表单数据的中间件
 app.use(express.urlencoded({ extended: false }))
 
+/* 用于处理客户端通过 JSONP 方式请求数据 */
 // 必须在配置 cors 中间件之前，配置 JSONP 的接口
 app.get('/api/jsonp', (req, res) => {
     // TODO: 定义 JSONP 接口具体的实现过程
@@ -19,13 +21,16 @@ app.get('/api/jsonp', (req, res) => {
     res.send(scriptStr)
 })
 
+/*  配置了跨域中间件，解决了接口跨域的问题 */
 // 一定要在路由之前，配置 cors 这个中间件，从而解决接口跨域的问题
 const cors = require('cors')
 app.use(cors())
 
-// 导入路由模块
+
+/* 自定义的路由模块 */
+// 导入路由模块  路由都写在apiRoute.js中
 const router = require('./apiRouter')
-// 把路由模块，注册到 app 上
+// 把路由模块，注册到 app 上、路由都经过/api前缀，比如http://localhost:3333/api/get
 app.use('/api', router)
 
 // 启动服务器
